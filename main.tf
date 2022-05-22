29da2ac2483b465f51abf1462cfc8720cbaabcc6
sureshrangagit <47292210+sureshrangagit@users.noreply.github.com>
2022-05-22 07:24:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
-resource "aws_instance" "server-1" {
-	ami = var.ami
-	instance_type = var.instance_type
-}
+variable "ami" {}
+variable "region" {}
+variable "instance_type" {}
+variable "access_id" {}
+variable "secret_key" {}
+variable "subnet_id" {}
 
+
+resource "aws_instance" "server-1" {
+
+	ami = var.ami
+	region = var.region
+	instance_type = var.instance_type
+	subnet_id = var.subnet_id
+}
 

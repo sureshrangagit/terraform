8ceec87efa12aa2a222a771cd0c62521f7879016
sureshrangagit <47292210+sureshrangagit@users.noreply.github.com>
2022-05-26 19:00:52 +0530

    Create main.tf

---
+++ first_ec2/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAUDMH7VNT4VXQX77X"
+  secret_key = "DB1k0ziDDVniJPPJH7hre9RTrQrM9xndixGGWPwGV"
+}
+
+resource "aws_instance" "myec2" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+}

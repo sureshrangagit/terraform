29454b8678a52140f3198d8730a341c0b2fd1a85
sureshrangagit <47292210+sureshrangagit@users.noreply.github.com>
2022-05-26 20:07:16 +0530

    Create sec_grp.tf

---
+++ sec_grp.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "secgroup1" {
+  name = "seclab_variables"
+  
+  ingress {
+    from_port = 443
+    to_port = 443
+    cidr_blocks = [var.vpn_ip]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    cidr_blocks = [var.vpn_ip]
+  }
+  ingress {
+    from_port = 53
+    to_port = 53
+    cidr_blocks = [var.vpn_ip]
+  }
+  

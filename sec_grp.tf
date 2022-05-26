0ab86d7e2f9ce4116e16def9814156702fa9878d
sureshrangagit <47292210+sureshrangagit@users.noreply.github.com>
2022-05-26 20:11:22 +0530

    Update sec_grp.tf

--- sec_grp.tf
+++ sec_grp.tf
@@ -4,17 +4,17 @@ resource "aws_security_group" "secgroup1" {
   ingress {
     from_port = 443
     to_port = 443
-    cidr_blocks = [var.vpn_ip]
+    cidr_blocks = [var.vpn_id]
   }
   ingress {
     from_port = 80
     to_port = 80
-    cidr_blocks = [var.vpn_ip]
+    cidr_blocks = [var.vpn_id]
   }
   ingress {
     from_port = 53
     to_port = 53
-    cidr_blocks = [var.vpn_ip]
+    cidr_blocks = [var.vpn_id]
   }
   
 }
